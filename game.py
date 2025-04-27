6de085b0f33d4266f28ec8dd4defd3f4f0fc41b3
dorpen6 <alivalishov20@gmail.com>
2025-04-27 23:16:20 +0200

    adding a loop that keeps the window open (forever, no way to exit)

--- game.py
+++ game.py
@@ -18,8 +18,18 @@ OUTLINE_THICKNESS = 10
 BACKGROUND_COLIR = (205, 192, 180)
 FONT_COLOR = (119, 110, 101)
 
+FONT = pygame.font.SysFont("comicsans", 60, bold=True)
+MOVE_VELOCITY = 20
+
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048")
 
-FONT = pygame.font.SysFont("comicsans", 60, bold=True)
-MOVE_VELOCITY = 20
+def main(window):
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+            clock.tick(FPS)
+
+if __name__ == '__main__':
+    main(WINDOW)
